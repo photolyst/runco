83a229efe66775121227c764bd66446941954661
susumu yagi <susumu.yagi@gmail.com>
2026-03-06 17:00:07 +0900

    add logo to sign-in page

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,9 +1,13 @@
+import { RuncoLogo } from "@/components/runco-logo";
 import { SignUpForm } from "@/components/sign-up-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-br from-primary via-[#f89c5b] to-[#41b2bd]">
-      <div className="w-full max-w-sm">
+    <div className="relative flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-br from-primary via-[#f89c5b] to-[#41b2bd]">
+      <div className="absolute top-3 left-6 md:top-3 md:left-6">
+        <RuncoLogo className="text-4xl md:text-5xl bg-[#fff5f2]" />
+      </div>
+      <div className="w-full max-w-sm mt-12 md:mt-0">
         <SignUpForm />
       </div>
     </div>
