e63607808f26f308c472407845179c59ace14bff
susumu yagi <susumu.yagi@gmail.com>
2026-03-18 17:11:45 +0900

    change protected getClaims -> getUser

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -6,15 +6,15 @@ import { createClient } from "@/lib/supabase/server";
 export default async function ProtectedPage() {
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.getClaims();
-  if (error || !data?.claims) {
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
     redirect("/auth/login");
   }
 
   return (
     <div className="flex h-svh w-full items-center justify-center gap-2">
       <p>
-        Hello <span>{data.claims.email}</span>
+        Hello <span>{data.user.email}</span>
       </p>
       <LogoutButton />
     </div>
